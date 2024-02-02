9147ca70c9d69b9be9084bd899140956e62cefc7
Apuroop-babu <122634154+Apuroop-babu@users.noreply.github.com>
2024-02-02 17:59:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,8 +69,8 @@ def user_input(user_question):
     st.write("Reply: ", response["output_text"])
 
 def main():
-    st.set_page_config("Blabber Mouth 🤖", layout="centered")
     st.set_page_config("BLABBERMOUTH")
+    st.title("Blabber Mouth     🤖")
     st.header("Upload your pdf files and start asking questions")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
